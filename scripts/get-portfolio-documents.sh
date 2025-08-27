498f4a6657a00953c3458a2c60b969de5602c083
Peter Aland <peter@iprogress.co.uk>
2025-08-27 10:16:43 +0100

    Refactor get-portfolio-documents.sh for improved variable handling and consistency

--- scripts/get-portfolio-documents.sh
+++ scripts/get-portfolio-documents.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-sed -i -e 's/\r$//' $script_path/config.sh
-source $script_path/config.sh
-source $script_path/functions.sh
+sed -i -e 's/\r$//' "${script_path}/config.sh"
+source "${script_path}/config.sh"
+source "${script_path}/functions.sh"
 
 bookingid=$1
 
-$script_path/control-tables.sh $ca_db_table_prefix_portfolio_document
-$script_path/control-tables.sh $ca_db_table_prefix_portfolio_attachment
+"${script_path}"/control-tables.sh "${ca_db_table_prefix:-0}_portfolio_document"
+"${script_path}"/control-tables.sh "${ca_db_table_prefix:-0}_portfolio_attachment"
 
-echo "bookingid = $bookingid"
+echo "bookingid = ${bookingid}"
 
-file=$script_path/xml/listportfoliodocuments-$bookingid.xml
+file=${script_path}/xml/listportfoliodocuments-$bookingid.xml
 
 xml='xml=<?xml version="1.0"?>
 <request xmlns="http://fusionapi.traveltek.net/1.0/xsds"> 
@@ -27,11 +27,11 @@ curl -o $file -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/l
 sed -i 's/xmlns="http:\/\/fusionapi.traveltek.net\/1.0\/xsds"/ /g' $file
 
 mysql --login-path=local --skip-column-names --local-infile --execute="USE "$ca_db_name";
-DELETE FROM "$ca_db_table_prefix"_portfolio_document WHERE bookingid = $bookingid;
+DELETE FROM ${ca_db_table_prefix:-0}_portfolio_document WHERE bookingid = $bookingid;
 
 LOAD XML LOCAL INFILE '$file' 
 REPLACE
-INTO TABLE "$ca_db_table_prefix"_portfolio_document
+INTO TABLE ${ca_db_table_prefix:-0}_portfolio_document
 ROWS IDENTIFIED BY '<document>'
 	(id,
 	documentid,
@@ -42,11 +42,11 @@ ROWS IDENTIFIED BY '<document>'
 SET 
 	bookingid = $bookingid;
 
-DELETE FROM "$ca_db_table_prefix"_portfolio_attachment WHERE bookingid = $bookingid;
+DELETE FROM ${ca_db_table_prefix:-0}_portfolio_attachment WHERE bookingid = $bookingid;
 
 LOAD XML LOCAL INFILE '$file' 
 REPLACE
-INTO TABLE "$ca_db_table_prefix"_portfolio_attachment
+INTO TABLE ${ca_db_table_prefix:-0}_portfolio_attachment
 ROWS IDENTIFIED BY '<attachment>'
 	(id,
 	documentid,
