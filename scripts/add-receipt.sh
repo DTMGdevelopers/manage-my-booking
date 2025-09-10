c32703239744bd2ff83dce8805f5e7fb396c1a4a
Peter Aland <peter@iprogress.co.uk>
2025-09-10 10:28:48 +0100

    Refactor add-receipt.sh to improve variable handling and add trading name ID in create document request

--- scripts/add-receipt.sh
+++ scripts/add-receipt.sh
@@ -8,15 +8,15 @@ source "${script_path}/config.sh"
 bookingid=${1}
 
 file=$script_path/xml/book-$bookingid.xml
-cardtype=$(xmlstarlet sel -t -m "/transaction/cardtype" -v . -n $file )
-amount=$(xmlstarlet sel -t -m "/transaction/amount" -v . -n $file )
-authcode=$(xmlstarlet sel -t -m "/transaction/authcode" -v . -n $file )
-reference=$(xmlstarlet sel -t -m "/transaction/reference" -v . -n $file )
-handlingfee=$(xmlstarlet sel -t -m "/transaction/handlingfee" -v . -n $file )
-transactionref=$(xmlstarlet sel -t -m "/transaction/transactionref" -v . -n $file )
-cardno=$(xmlstarlet sel -t -m "/transaction/cardno" -v . -n $file )
+cardtype=$(xmlstarlet sel -t -m "/transaction/cardtype" -v . -n "${file}" )
+amount=$(xmlstarlet sel -t -m "/transaction/amount" -v . -n "${file}" )
+authcode=$(xmlstarlet sel -t -m "/transaction/authcode" -v . -n "${file}" )
+reference=$(xmlstarlet sel -t -m "/transaction/reference" -v . -n "${file}" )
+handlingfee=$(xmlstarlet sel -t -m "/transaction/handlingfee" -v . -n "${file}" )
+transactionref=$(xmlstarlet sel -t -m "/transaction/transactionref" -v . -n "${file}" )
+cardno=$(xmlstarlet sel -t -m "/transaction/cardno" -v . -n "${file}" )
 
-paymentmethodid=$(mysql --login-path=local --skip-column-names --local-infile --execute="USE "$ca_db_name";
+paymentmethodid=$(mysql --login-path=local --skip-column-names --local-infile --execute="USE ${ca_db_name:-0};
 SELECT id FROM ${ca_db_table_prefix:-0}_payment_methods WHERE creditcardcode = '$cardtype';")
 
 #if we dont get a paymentmrthodid than just use 6389 as its teh default from TravelTek
@@ -32,27 +32,28 @@ fi
 
 xml='xml=<?xml version="1.0"?>
   <request xmlns="http://fusionapi.traveltek.net/1.0/xsds">
-    <auth username="'${ca_tt_username}'" password="'${ca_tt_password}'" />
-    <method action="addreceipt" sitename="'${ca_tt_sitename:-0}'" bookingid="'${bookingid}'" paymentmethodid="'${paymentmethodid}'" creditvalue="'${amount}'" authcode="'${authcode}'" reference="'${reference}'" handlingfee="'${handlingfee}'" transactionref="'${transactionref}'" cardno="'${cardno}'" useportfoliobranch="1" sendemail="1" />
+    <auth username="'${ca_tt_username:-0}'" password="'${ca_tt_password:-0}'" />
+    <method action="addreceipt" sitename="'${ca_tt_sitename:-0}'" bookingid="'${bookingid}'" paymentmethodid="'${paymentmethodid}'" creditvalue="'${amount}'" authcode="'${authcode}'" reference="'${reference}'" handlingfee="'${handlingfee}'" transactionref="'${transactionref}'" cardno="'${cardno}'" useportfoliobranch="1" />
   </request>'
 
 file=$script_path/xml/addreceipt-$bookingid.xml
 
-curl -o $file -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/addreceipt" \
+curl -o "${file}" -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/addreceipt" \
     -H "Content-Type: application/x-www-form-urlencoded" \
     -d "$xml"
 
 #CREATE THE DOCUMENT
+# Hard coded values need to be replaced with dynamic values if possible in the future via the config file, OR, use the sendemai
 xml='xml=<?xml version="1.0"?>
   <request xmlns="http://fusionapi.traveltek.net/1.0/xsds">
     <auth username="'${ca_tt_username}'" password="'${ca_tt_password}'" />
-    <method action="createdocument" sitename="'${ca_tt_sitename}'" bookingid="'${bookingid}'" documentid="70894">
+    <method action="createdocument" sitename="'${ca_tt_sitename}'" bookingid="'${bookingid}'" documentid="70894" tradingnameid="5708" >
       <attachments/>
     </method>
   </request>'
 
 file=$script_path/xml/createdocument.xml
 
-curl -o $file -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/createdocument" \
+curl -o "${file}" -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/createdocument" \
     -H "Content-Type: application/x-www-form-urlencoded" \
     -d "$xml"
