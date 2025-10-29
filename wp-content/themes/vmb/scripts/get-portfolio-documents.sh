91736101266786033c1039ad22ab9655fad2f2ab
Peter Aland <peter@iprogress.co.uk>
2025-10-29 08:32:25 +0000

    Fix token retrieval and update request ID casing in portfolio document script

--- wp-content/themes/vmb/scripts/get-portfolio-documents.sh
+++ wp-content/themes/vmb/scripts/get-portfolio-documents.sh
@@ -20,7 +20,7 @@ xml='xml=<?xml version="1.0"?>
     <method action="listportfoliodocuments" sitename="'${ca_tt_sitename:-0}'" bookingid="'${bookingid}'" externalreference="" />
 </request>'
 
-curl -o "$file" -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/listportfoliodocuments" \
+curl -o "${file}" -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl/listportfoliodocuments" \
 		-H "Content-Type: application/x-www-form-urlencoded" \
 		-d "$xml" 
 
@@ -28,14 +28,15 @@ curl -o "$file" -X POST --url "https://fusionapi.traveltek.net/1.0/backoffice.pl
 sed -i 's/xmlns="http:\/\/fusionapi.traveltek.net\/1.0\/xsds"/ /g' "$file"
 
 #MARK: GET AUTH TOKEN
+token_file=$script_path/json/token.json
 ca_tt_auth_b64="$(echo -n "${ca_tt_username}:${ca_tt_password}" | base64)"
 mkdir -p "${script_path}/json"
-curl -s -o "$file" -X POST --url "https://fusionapi.traveltek.net/2.0/json/token.pl" \
+curl -s -o "$token_file" -X POST --url "https://fusionapi.traveltek.net/2.0/json/token.pl" \
 		-H  "Content-Type: application/x-www-form-urlencoded" \
 		-H  "Authorization: Basic ${ca_tt_auth_b64}" \
 		--data-urlencode "grant_type=client_credentials" \
 		--data-urlencode "scope=docdownload"
-token=$(jq -r '.access_token' "$file")
+token=$(jq -r '.access_token' "$token_file")
 
 #MARK: IMPORT DATA INTO DATABASE
 mysql --login-path=local --skip-column-names --local-infile --execute="USE ${ca_db_name:-0};
@@ -54,7 +55,7 @@ ROWS IDENTIFIED BY '<document>'
 SET 
 	bookingid = $bookingid;
 
-UPDATE ${ca_db_table_prefix:-0}_portfolio_document SET link = CONCAT(link,'&requestID=${token}') WHERE bookingid = $bookingid;
+UPDATE ${ca_db_table_prefix:-0}_portfolio_document SET link = CONCAT(link,'&requestid=${token}') WHERE bookingid = $bookingid;
 
 DELETE FROM ${ca_db_table_prefix:-0}_portfolio_attachment WHERE bookingid = $bookingid;
 
@@ -70,4 +71,4 @@ ROWS IDENTIFIED BY '<attachment>'
 SET 
 	bookingid = $bookingid;
 
-UPDATE ${ca_db_table_prefix:-0}_portfolio_attachment SET link = CONCAT(link,'&requestID=${token}') WHERE bookingid = $bookingid;"
+UPDATE ${ca_db_table_prefix:-0}_portfolio_attachment SET link = CONCAT(link,'&requestid=${token}') WHERE bookingid = $bookingid;"
